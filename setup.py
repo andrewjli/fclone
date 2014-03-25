428bda1dd6f7feb2f259e351334e4e4e16c9ab4a
southrop <southrop113@gmail.com>
2014-03-25 23:54:13 +0000

    Improved build process

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['fclone.py'])
+setup(
+	name="FClone",
+	version="0.1",
+	description="A simple code similarity detector for Java source code",
+	console=['fclone.py'],
+	data_files=[('',[
+		'README.md',
+		'preprocess.pl'
+	])],
+)
