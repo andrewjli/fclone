b0b23d73ae8af0525ee9d9a67ca0aae7fb0a2ec3
Andrew Li <andrewli92@gmail.com>
2014-03-25 23:21:41 +0000

    Tab/space consistency

--- preprocess.pl
+++ preprocess.pl
@@ -2,7 +2,7 @@
 #read in the whole file as a string
 #get the file argument
 foreach my $file (@ARGV){
-	$fileName = $file;
+    $fileName = $file;
 }
 local $/=undef;
 open(FILE, "$fileName.java");
@@ -32,59 +32,59 @@ close FILE;
 # java.lang.Integer becomes Integer
 # system.out.println becomes println
 sub deQualify{
-	my $text = $_[0];
-	my @matches = $text =~ m/(\w+\.\w+[\.\w+]+)/g;
-	foreach (@matches){
-		@haystack = split('\.', $_);
-		$deQualified = pop (@haystack);
-		$text =~ s/$_/$deQualified/g;
-		
-	}
-	return $text;
+    my $text = $_[0];
+    my @matches = $text =~ m/(\w+\.\w+[\.\w+]+)/g;
+    foreach (@matches){
+        @haystack = split('\.', $_);
+        $deQualified = pop (@haystack);
+        $text =~ s/$_/$deQualified/g;
+
+    }
+    return $text;
 }
 
 sub breakVariables{
-	my $text = $_[0];
-	#the whole thing
-	my @matches = $text =~ m/(\w+ \s*\w+\s*[\s*,\s*\w+]+;)/g;
-	foreach (@matches){	
-		#need to get the first word, which is the type
-		$match = $_;
-		$a =$_ =~ /^(.*?)\s/;
-		$type = $1;
-	#	print("type: $type \n");
-		$variableStr = $_;
-		$variableStr =~  s/$type//g;
-		#print("variables: $variableStr\n");
-		chop($variableStr);
-		@variables = split(',', $variableStr);
-		$replacement = '';
-		foreach(@variables){
-		#	$variable = pop(@variables);
-#			print("$type $_ = 9 \n");
-			$replacement .= $type.' '.$_.";\n"; 
+    my $text = $_[0];
+    #the whole thing
+    my @matches = $text =~ m/(\w+ \s*\w+\s*[\s*,\s*\w+]+;)/g;
+    foreach (@matches){
+        #need to get the first word, which is the type
+        $match = $_;
+        $a =$_ =~ /^(.*?)\s/;
+        $type = $1;
+    #   print("type: $type \n");
+        $variableStr = $_;
+        $variableStr =~  s/$type//g;
+        #print("variables: $variableStr\n");
+        chop($variableStr);
+        @variables = split(',', $variableStr);
+        $replacement = '';
+        foreach(@variables){
+        #   $variable = pop(@variables);
+#           print("$type $_ = 9 \n");
+            $replacement .= $type.' '.$_.";\n";
 
-		}
-		#replace line with multi-line declarations
-		$text =~ s/$match/$replacement/g;	
+        }
+        #replace line with multi-line declarations
+        $text =~ s/$match/$replacement/g;
 
-	}	
-	return $text;
+    }
+    return $text;
 
 }
 
 sub breakAssigments(){
-	 $text = $_[0];	
-	my @matches = $text =~  m/(\w+\s*=\s*\w+[\s*=\s*\w+]+;)/g;
-	foreach( @matches){
-		@haystack = split('=', $_);	
-		$literal = pop(@haystack);
-		my $replacement = '';
-		foreach(@haystack){
-			$replacement .= "$_ = $literal\n";
-		}
-		$text =~ s/$_/$replacement/g;	
-	}
-	return $text;
+     $text = $_[0];
+    my @matches = $text =~  m/(\w+\s*=\s*\w+[\s*=\s*\w+]+;)/g;
+    foreach( @matches){
+        @haystack = split('=', $_);
+        $literal = pop(@haystack);
+        my $replacement = '';
+        foreach(@haystack){
+            $replacement .= "$_ = $literal\n";
+        }
+        $text =~ s/$_/$replacement/g;
+    }
+    return $text;
 
 }
